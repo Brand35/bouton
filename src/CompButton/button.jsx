b8e0925f3573124426fb980a9f6084ed67289dea
Brand35 <brandou35000@gmail.com>
2024-06-26 15:39:34 +0200

    1

--- src/CompButton/button.jsx
+++ src/CompButton/button.jsx
@@ -1,4 +1,5 @@
 import { React, useState } from "react";
+import './style.nodule.css';
 
 function Button() {
   const [counter, setCounter] = useState(0);
@@ -10,7 +11,7 @@ function Button() {
 
   //decrease counter
   const decrease = () => {
-    if (counter > 0) {
+    if (counter > 0) { 
       setCounter((count) => count - 1);
     }
   };
@@ -18,13 +19,14 @@ function Button() {
   const reset = () => {
     setCounter(0);
   };
-
+// en render je veux afficher le compteur au milieur par un span  suivie des bouttons + / - / reset 
+// j'ai utiliser la condition if pour definir que le compteur ne pass pas la bar des zero.
   return (
     <div className="counter">
-    <h1>React Counter</h1>
-    <span className="counter__output">{counter}</span>
+    <h1>compteur</h1>
+    <span className="counter__output">{counter}</span> 
     <div className="btn__container">
-      <button className="control__btn" onClick={increase}>+</button>
+      <button className="control__btn" onClick={increase}>+</button> 
       <button className="control__btn" onClick={decrease}>-</button>
       <button className="reset" onClick={reset}>Reset</button>
     </div>
